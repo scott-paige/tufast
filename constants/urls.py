08d14efe75041fae2a6e282f8a6c16bc16aeda54
scottpaige <scott.paige@csaa.com>
2019-04-13 09:18:43 -0700

    Updated catbot to use a new api with space cats ;)

--- constants/urls.py
+++ constants/urls.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 weatherBaseURL = 'https://wttr.in'
 trumpQuoteURL = 'https://api.tronalddump.io/random/quote'
-catPicURL = 'https://aws.random.cat/meow'
+catPicURL = 'https://api.thecatapi.com/v1/images/search'
 dogPicURL = 'https://random.dog/woof.json'
 newsURL = ('https://newsapi.org/v2/top-headlines?'
                 'country=us&'
-                'apiKey={}'.format(os.environ["NEWS_API"]))
+                f'apiKey={os.environ["NEWS_API"]}')
 coinGeckoBaseURL = 'https://api.coingecko.com/api/v3'
